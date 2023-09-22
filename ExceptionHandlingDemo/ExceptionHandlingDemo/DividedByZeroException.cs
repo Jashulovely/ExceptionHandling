fe93b9285397153fe844efcb623662fc08c065a5
jashulovely <porsu.jaswanthkumar@gmail.com>
2023-09-22 09:38:49 +0530

    [Jashu]Add : DividedByZeroException

---
+++ ExceptionHandlingDemo/ExceptionHandlingDemo/DividedByZeroException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandlingDemo
+{
+    internal class DividedByZeroException
+    {
+        public void Divide()
+        {
+            int a = 0;
+            int b = 10;
+            //if (a != 0)
+            //{
+            //    int b = 10 / a;
+            //}
+            try
+            {
+                int c = b/a;
+            }
+            catch(DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.WriteLine("Hi");
+        }
+    }
+}
---
+++ ExceptionHandlingDemo/ExceptionHandlingDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandlingDemo
+{
+    // a problem that occurs during the execution of a program is refered to as an exception
+    // its allow to control of apast from one part of the program to the another exception.
+    // it will prevent the crashing of the program 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DividedByZeroException dividedByZeroException = new DividedByZeroException();
+            dividedByZeroException.Divide();
+            Console.ReadLine();
+        }
+    }
+}
